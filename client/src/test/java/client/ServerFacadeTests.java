825f9269295c03006a978c454d491170efa989aa
nathanyjones <nathanyjones@gmail.com>
2025-03-25 19:30:02 -0600

    Fixing code style for tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -18,9 +18,9 @@ public class ServerFacadeTests {
     private static Server server;
     private static ServerFacade facade;
 
-    final private static UserData user1 = new UserData("user1", "12345", "u1@mail.com");
-    final private static UserData duplicateUser1 = new UserData("user1", "54321", "u11@mail.com");
-    final private static String invalidAuthToken = "invalid_auth_token";
+    final private static UserData USER_1 = new UserData("user1", "12345", "u1@mail.com");
+    final private static UserData DUPLICATE_USER_1 = new UserData("user1", "54321", "u11@mail.com");
+    final private static String INVALID_AUTH_TOKEN = "invalid_auth_token";
 
     @BeforeAll
     public static void init() {
@@ -47,7 +47,7 @@ public class ServerFacadeTests {
     @Test
     public void registerSuccessTest() {
         try {
-            AuthData authData = facade.register(user1);
+            AuthData authData = facade.register(USER_1);
             assertEquals("user1", authData.username());
             assertNotNull(authData.authToken());
         } catch (ResponseException e) {
@@ -58,15 +58,15 @@ public class ServerFacadeTests {
     @Test
     public void registerFailUserExistsTest() {
         assertThrows(ResponseException.class, () -> {
-            facade.register(user1);
-            facade.register(duplicateUser1);
+            facade.register(USER_1);
+            facade.register(DUPLICATE_USER_1);
         });
     }
 
     @Test
     public void loginSuccessTest() {
         try {
-            AuthData authData = facade.register(user1);
+            AuthData authData = facade.register(USER_1);
             assertEquals("user1", authData.username());
             assertNotNull(authData.authToken());
         } catch (ResponseException e) {
@@ -77,15 +77,15 @@ public class ServerFacadeTests {
     @Test
     public void loginFailInvalidPasswordTest() {
         assertThrows(ResponseException.class, () -> {
-            facade.register(user1);
-            facade.login(duplicateUser1);
+            facade.register(USER_1);
+            facade.login(DUPLICATE_USER_1);
         });
     }
 
     @Test
     public void createGameSuccessTest() {
         try {
-            AuthData authData = facade.register(user1);
+            AuthData authData = facade.register(USER_1);
             Integer gameID = facade.createGame(authData.authToken(), "Game 1");
             assertNotNull(gameID);
         } catch (ResponseException e) {
@@ -95,14 +95,14 @@ public class ServerFacadeTests {
 
     @Test
     public void createGameFailInvalidAuthTest() {
-        assertThrows(ResponseException.class, () -> facade.createGame(invalidAuthToken, "Game 1"));
+        assertThrows(ResponseException.class, () -> facade.createGame(INVALID_AUTH_TOKEN, "Game 1"));
     }
 
     @Test
     public void logoutSuccessCreateGameTest() {
         String authToken;
         try {
-            authToken = facade.register(user1).authToken();
+            authToken = facade.register(USER_1).authToken();
             facade.logout(authToken);
         } catch (ResponseException e) {
             fail("Unexpected ResponseException from Register or Logout Method: " + e.getMessage());
@@ -114,18 +114,18 @@ public class ServerFacadeTests {
     @Test
     public void logoutFailInvalidAuthTokenTest() {
         try {
-            facade.register(user1);
+            facade.register(USER_1);
         } catch (ResponseException e) {
             fail("Unexpected ResponseException from Register Method: " + e.getMessage());
             return;
         }
-        assertThrows(ResponseException.class, () -> facade.logout(invalidAuthToken));
+        assertThrows(ResponseException.class, () -> facade.logout(INVALID_AUTH_TOKEN));
     }
 
     @Test
     public void joinGameSuccessTest() {
         try {
-            AuthData authData = facade.register(user1);
+            AuthData authData = facade.register(USER_1);
             Integer gameID = facade.createGame(authData.authToken(), "Game 1");
             facade.joinGame(authData.authToken(), gameID, "WHITE");
         } catch (ResponseException e) {
@@ -138,7 +138,7 @@ public class ServerFacadeTests {
         AuthData authData;
         Integer gameID;
         try {
-            authData = facade.register(user1);
+            authData = facade.register(USER_1);
             gameID = facade.createGame(authData.authToken(), "Game 1");
         } catch (ResponseException e) {
             fail("Unexpected ResponseException from Registration / Creating Game: " + e.getMessage());
@@ -151,7 +151,7 @@ public class ServerFacadeTests {
     @Test
     public void listGamesSuccessTest() {
         try {
-            AuthData authData = facade.register(user1);
+            AuthData authData = facade.register(USER_1);
             Integer gameID = facade.createGame(authData.authToken(), "Game 1");
             ArrayList<GameData> games = (ArrayList<GameData>) facade.listGames(authData.authToken());
             assertNotNull(games);
@@ -164,19 +164,19 @@ public class ServerFacadeTests {
 
     @Test
     public void listGamesFailInvalidAuthTest() {
-        assertThrows(ResponseException.class, () -> facade.listGames(invalidAuthToken));
+        assertThrows(ResponseException.class, () -> facade.listGames(INVALID_AUTH_TOKEN));
     }
 
     @Test
     public void clearSuccessTest() {
         try {
-            String authToken = facade.register(user1).authToken();
+            String authToken = facade.register(USER_1).authToken();
             facade.createGame(authToken, "Game 1");
             facade.createGame(authToken, "Game 2");
             int numGames = facade.listGames(authToken).size();
             assertEquals(2, numGames);
             facade.clear();
-            authToken = facade.register(user1).authToken();
+            authToken = facade.register(USER_1).authToken();
             Collection<GameData> games = facade.listGames(authToken);
             assertEquals(0, games.size());
         } catch (ResponseException e) {
@@ -187,7 +187,7 @@ public class ServerFacadeTests {
     @Test
     public void getGameSuccessTest() {
         try {
-            AuthData authData = facade.register(user1);
+            AuthData authData = facade.register(USER_1);
             Integer gameID = facade.createGame(authData.authToken(), "Game 1");
             GameData gameData = facade.getGame(authData.authToken(), gameID);
             assertNotNull(gameData);
@@ -201,7 +201,7 @@ public class ServerFacadeTests {
     @Test
     public void getGameFailInvalidIDTest() {
         assertThrows(ResponseException.class, () -> {
-            String authToken = facade.register(user1).authToken();
+            String authToken = facade.register(USER_1).authToken();
             facade.getGame(authToken, -1);
         });
     }
