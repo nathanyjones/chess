53b0b78eeef1024b9e144275c5773417573bc128
nathanyjones <nathanyjones@gmail.com>
2025-03-25 21:46:27 -0600

    Restoring ListGamesResult.java

---
+++ shared/src/main/java/result/ListGamesResult.java
@@ -0,0 +1,28 @@
+package result;
+
+import model.GameData;
+import java.util.Collection;
+
+public class ListGamesResult {
+    private final Collection<GameData> games;
+    private final String message;
+
+    public ListGamesResult(Collection<GameData> games) {
+        this.games = games;
+        this.message = null;
+    }
+
+    public ListGamesResult(String message) {
+        this.message = message;
+        this.games = null;
+    }
+
+    public Collection<GameData> getGameList() {
+        return this.games;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+}
